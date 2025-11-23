401eb1cc6fe0b39f21bcfb8a55267277fd986938
namanpandey-016 <namankraj@gmail.com>
2025-11-24 02:49:49 +0530

    Add number determination script

---
+++ Day1-NumDet.py
@@ -0,0 +1,9 @@
+# Day1-Num-determination
+#Num determination
+num = int(input("Enter the number"))
+if num > 0 :
+    print("Number is positive")
+elif num < 0 :
+    print("Number is Negative")
+else :
+    print("Number is Zero")
